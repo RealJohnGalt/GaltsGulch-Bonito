ae182415a20044f90a9ba03f351bcd71a9106e7e
John Galt <johngaltfirstrun@gmail.com>
2020-11-06 14:00:59 -0500

    block/blk-settings: fix build

--- block/blk-settings.c
+++ block/blk-settings.c
@@ -682,7 +682,7 @@ void disk_stack_limits(struct gendisk *disk, struct block_device *bdev,
 		       top, bottom);
 	}
 
-	t->backing_dev_info.io_pages =
+	t->backing_dev_info->io_pages =
 		t->limits.max_sectors >> (PAGE_SHIFT - 9);
 }
 EXPORT_SYMBOL(disk_stack_limits);
